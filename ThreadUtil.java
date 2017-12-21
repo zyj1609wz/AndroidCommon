8e9d04e3cd51036e0c741e9c931810361d5cd77f
赵彦军 <362299465@qq.com>
2017-12-21 18:17:10 +0800

    Add files via upload

---
+++ ThreadUtil.java
@@ -0,0 +1,42 @@
+package com.yiba.wifi.sdk.lib.util;
+
+import android.os.Looper;
+
+/**
+ * Created by ${zyj} on 2016/8/25.
+ */
+public class ThreadUtil {
+
+    /**
+     * Throws an {@link IllegalArgumentException} if called on a thread other than the main thread.
+     */
+    public static void assertMainThread() {
+        if (!isOnMainThread()) {
+            throw new IllegalArgumentException("You must call this method on the main thread");
+        }
+    }
+
+    /**
+     * Throws an {@link IllegalArgumentException} if called on the main thread.
+     */
+    public static void assertBackgroundThread() {
+        if (!isOnBackgroundThread()) {
+            throw new IllegalArgumentException("You must call this method on a background thread");
+        }
+    }
+
+    /**
+     * Returns {@code true} if called on the main thread, {@code false} otherwise.
+     */
+    public static boolean isOnMainThread() {
+        return Looper.getMainLooper().getThread() == Thread.currentThread();
+    }
+
+    /**
+     * Returns {@code true} if called on the main thread, {@code false} otherwise.
+     */
+    public static boolean isOnBackgroundThread() {
+        return !isOnMainThread();
+    }
+
+}
